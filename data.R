9c1f7de1cdcb96583421c26e7da1c45d8e532f97
Anton Ivanov <antuan.ivanov@gmail.com>
2021-12-12 10:01:54 +0100

    last commit

--- data.R
+++ data.R
@@ -2,25 +2,33 @@
 # Predicting mortality caused by Heart Failure
 ################################################
 
-library(tidyverse)
-library(caret)
-library(psych)
-library(reshape2)
-library(gridExtra)
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(psych)) install.packages("psych", repos = "http://cran.us.r-project.org")
+if(!require(reshape2)) install.packages("reshape2", repos = "http://cran.us.r-project.org")
+if(!require(gridExtra)) install.packages("gridExtra", repos = "http://cran.us.r-project.org")
+if(!require(klaR)) install.packages("klaR", repos = "http://cran.us.r-project.org")
+if(!require(kernlab)) install.packages("kernlab", repos = "http://cran.us.r-project.org")
+if(!require(rpart)) install.packages("rpart", repos = "http://cran.us.r-project.org")
+if(!require(e1071)) install.packages("e1071", repos = "http://cran.us.r-project.org")
+if(!require(fastAdaboost)) install.packages("fastAdaboost", repos = "http://cran.us.r-project.org")
+if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
+if(!require(nnet)) install.packages("nnet", repos = "http://cran.us.r-project.org")
+options(digits=3)
 
-# Dataset available from:
+# Data set available from:
 # Heart failure clinical records. (2020). UCI Machine Learning Repository.
 # https://archive.ics.uci.edu/ml/machine-learning-databases/00519/
 
 ############################
-# Preparation of the dataset
+# Preparation of the data set
 ############################
 
 data <- read.csv("heart_failure_clinical_records_dataset.csv")
 
 # Time column will not be considered for the prediction since this information
 # will not be available for real patients
-data <- select(data, -time)
+data <- dplyr::select(data, -time)
 data_num <- data
 
 # Binary variables are converted to factors, numeric variables are scaled
@@ -32,18 +40,17 @@ data_sc[-factors] <- sapply(data[-factors], scale)
 # Output variable levels are converted to "No"/"Yes" as needed for some methods
 levels(data_sc$DEATH_EVENT) = c("No","Yes")
 
-# Train and test datasets are created
+# Train and test data sets are created
 set.seed(2)
 train_index <- createDataPartition(data$DEATH_EVENT, p = 0.8, list = FALSE)
 train_not_sc <- train <- data[train_index,]
 train <- data_sc[train_index,]
 test <- data_sc[-train_index,]
 
-# The proportion of negative and positive outocmes in the train and test sets
+# The proportion of negative and positive outcomes in the train and test sets
 # is almost identical
 prop.table(table(train$DEATH_EVENT))
 prop.table(table(test$DEATH_EVENT))
-prop.table(table(train$sex))
 
 ###################################
 # Data exploration of the train set
@@ -54,6 +61,7 @@ str(train_not_sc)
 # Distribution and correlations of all features
 pairs.panels(train)
 
+# Correlation heatmap
 train_num <- data_num[train_index,]
 cormat <- round(cor(train_num),2)
 melted_cormat <- melt(cormat)
@@ -140,19 +148,12 @@ train_not_sc %>%
   geom_histogram(aes(y = ..density../2)) +
   geom_density(alpha = 0.2)
 
-# Sex
+# Sex and Smoking
 
 p1 <- train_not_sc %>%
   ggplot(aes(sex, fill = DEATH_EVENT)) +
   theme_gray() +
   geom_bar(position = "fill")
-
-p3 <- train_not_sc %>%
-  ggplot(aes(sex, fill = smoking)) +
-  theme_gray() +
-  geom_bar(position = "fill")
-
-# Smoking
 
 p2 <- train_not_sc %>%
   ggplot(aes(smoking, fill = DEATH_EVENT)) +
@@ -160,6 +161,11 @@ p2 <- train_not_sc %>%
   geom_bar(position = "fill")
 
 grid.arrange(p1, p2, nrow = 1)
+
+train_not_sc %>%
+  ggplot(aes(sex, fill = smoking)) +
+  theme_gray() +
+  geom_bar(position = "fill")
 
 #################
 # Training models
@@ -191,8 +197,8 @@ f1 <- function(data, lev = NULL, model = NULL) {
 # 5-fold cross-validation will be used to evaluate the models
 
 fitControl <- trainControl(method = "cv",
-                           number = 10,
-                           p = 0.9,
+                           number = 5,
+                           p = 0.8,
                            classProbs = TRUE,
                            summaryFunction = f1)
 
@@ -215,13 +221,11 @@ calc_model <- function(method, tuneGrid) {
   cm <- confusionMatrix(pred, train$DEATH_EVENT, mode = "everything", positive = "Yes")
   accuracy <- cm$overall["Accuracy"]
   F1 <- cm$byClass["F1"]
-  specificity <- cm$byClass["Specificity"]
-  kappa <- cm$overall["Kappa"]
   results <- list()
   # The functions returns a list of trained models, confusion matrices and a dataframe with metrics
   results[[1]] <- fit
   results[[2]] <- cm
-  results[[3]] <- data.frame(method = method, F1_train = F1_train, F1 = F1, accuracy = accuracy, specificity = specificity, kappa = kappa)
+  results[[3]] <- data.frame(method = method, F1_train = F1_train, F1 = F1, accuracy = accuracy)
   return(results)
 }
 
@@ -229,31 +233,42 @@ calc_model <- function(method, tuneGrid) {
 
 tuneGrids <- list()
 
+#Naive Bayes
 tuneGrids[[1]] <- expand.grid(fL = seq(0, 5, 1), 
                               usekernel = c(TRUE, FALSE), 
                               adjust = seq(0, 5, 1))
 
+# Logistic regression
 tuneGrids[2] <- list(NULL)
 
+# K-nearest neighbors
 tuneGrids[[3]] <- data.frame(k = seq(1, 99, 2))
 
+# SVM Linear
 tuneGrids[[4]] <- data.frame(C = seq(0.5, 10, 0.5))
 
+# SVM Radial
 tuneGrids[5] <- list(NULL)
 
+# SVM Polynomial
 tuneGrids[6] <- list(NULL)
 
+# Decision tree
 tuneGrids[[7]] <- data.frame(cp = seq(0, 0.05, len = 10))
 
+# Bagged decision tree
 tuneGrids[8] <- list(NULL)
 
+# Boosted decision tree
 tuneGrids[[9]] <- data.frame(nIter = seq(1, 19, 2),
-                            method = "M1")
+                             method = "M1")
 
+# Random forest
 tuneGrids[[10]] <- data.frame(mtry = seq(1, 11, 1))
 
+# ANN
 tuneGrids[[11]] <- expand.grid(size = seq(1, 10, 1),
-               decay = seq(1, 10, 1))
+                               decay = seq(1, 10, 1))
 
 
 # Models are trained and saved in a list
@@ -261,109 +276,23 @@ tuneGrids[[11]] <- expand.grid(size = seq(1, 10, 1),
 all_models <- mapply(calc_model, methods, tuneGrids)
 
 # Dataframe containing all methods and metrics is created
-df2 <- bind_rows(all_models[seq(3, 33, 3)], .id = "column_label")
-rownames(df2) <- NULL
-df2 <- df2[, -1]
-df2
 
-# All models are considered separately
-
-# Naive Bayes
-
-plot(all_models[[1]])
-
-all_models[[2]]$table
-
-#("nb", "glm", "knn", "svmLinear", "svmRadial", "svmPoly", "rpart", "treebag", "adaboost", "rf", "nnet")
-
-# Logistic regression
-
-all_models[[5]]$table
-
-# K-nearest neighbors
-
-plot(all_models[[7]])
-
-all_models[[8]]$table
-
-# SVM Linear
-
-plot(all_models[[10]])
-
-all_models[[11]]$table
-
-
-# SVM Radial
-
-plot(all_models[[13]])
-
-all_models[[14]]$table
-
-# SVM Polynomial
-
-plot(all_models[[16]])
-
-all_models[[17]]$table
-
-# Decision tree
-
-plot(all_models[[19]])
-
-all_models[[20]]$table
-
-# Bagged decision tree
-
-all_models[[23]]$table
-
-# Boosted decision tree
-
-plot(all_models[[25]])
-
-all_models[[26]]$table
-
-# Random forest
-
-plot(all_models[[28]])
-
-all_models[[29]]$table
-
-# ANN
-
-plot(all_models[[31]])
-
-all_models[[32]]$table
-
-# Tuning parameters are redefined for SVM linear, Boosted decision tree and Random forest
-
-tuneGrids[[4]] <- data.frame(C = seq(0.5, 100, 0.5))
-
-tuneGrids[[9]] <- data.frame(nIter = seq(1, 19, 2),
-                             method = "M1")
-
-tuneGrids[[10]] <- data.frame(mtry = seq(1, 20, 1))
-
-all_models <- mapply(calc_model, methods, tuneGrids)
-
-# Tuning results are checked again
-# SVM Linear
-
-plot(all_models[[10]])
-
-# Random forest
-
-plot(all_models[[28]])
+df <- bind_rows(all_models[seq(3, 33, 3)], .id = "column_label")
+rownames(df) <- NULL
+df <- df[, -1]
+df
 
 # Checking if reduction of features improves prediction
 
 calc_model_red <- function(method, tuneGrid) {
   set.seed(3)
   fit <- train(DEATH_EVENT ~ age + anaemia + ejection_fraction + serum_creatinine +
-                serum_sodium, # only features with correlation >= 0.05 are left
-                data = train,
-                method = method,
-                metric = "F1",
-                trControl = fitControl,
-                tuneGrid = tuneGrid)
+                 serum_sodium, # only features with correlation >= 0.05 are left
+               data = train,
+               method = method,
+               metric = "F1",
+               trControl = fitControl,
+               tuneGrid = tuneGrid)
   # F1-score for the train set will be used to evaluate models prior to evaluation 
   # based on the test set
   F1_train <- max(fit$results$F1, na.rm = TRUE)
@@ -373,13 +302,11 @@ calc_model_red <- function(method, tuneGrid) {
   cm <- confusionMatrix(pred, train$DEATH_EVENT, mode = "everything", positive = "Yes")
   accuracy <- cm$overall["Accuracy"]
   F1 <- cm$byClass["F1"]
-  specificity <- cm$byClass["Specificity"]
-  kappa <- cm$overall["Kappa"]
   results <- list()
   # The functions returns a list of trained models, confusion matrices and a dataframe with metrics
   results[[1]] <- fit
   results[[2]] <- cm
-  results[[3]] <- data.frame(method = method, F1_train = F1_train, F1 = F1, accuracy = accuracy, specificity = specificity, kappa = kappa)
+  results[[3]] <- data.frame(method = method, F1_train = F1_train, F1 = F1, accuracy = accuracy)
   return(results)
 }
 
@@ -388,31 +315,130 @@ all_models_red <- mapply(calc_model_red, methods, tuneGrids)
 df_red <- bind_rows(all_models_red[seq(3, 33, 3)], .id = "column_label")
 rownames(df_red) <- NULL
 
-# df_red <- df_red[, c(2, 3)]
+df_red <- df_red[, -1]
 df_red
+
+
+# All models are considered separately
+
+# Naive Bayes
+
+plot(all_models[[1]])
+all_models_red[[3]]$F1_train
+all_models[[2]]$table
+
+# Logistic regression
+
+all_models_red[[6]]$F1_train
+all_models[[5]]$table
+
+# K-nearest neighbors
+
+plot(all_models[[7]])
+all_models_red[[9]]$F1_train
+all_models[[8]]$table
+
+# SVM Linear
+
+plot(all_models[[10]])
+all_models_red[[12]]$F1_train
+all_models[[11]]$table
+
+
+# SVM Radial
+
+plot(all_models[[13]])
+all_models_red[[15]]$F1_train
+all_models[[14]]$table
+
+# SVM Polynomial
+
+plot(all_models[[16]])
+all_models_red[[18]]$F1_train
+all_models[[17]]$table
+
+# Decision tree
+
+plot(all_models[[19]])
+all_models_red[[21]]$F1_train
+all_models[[20]]$table
+
+# Bagged decision tree
+
+all_models_red[[24]]$F1_train
+all_models[[23]]$table
+
+# Boosted decision tree
+
+plot(all_models[[25]])
+all_models_red[[27]]$F1_train
+all_models[[26]]$table
+
+# Random forest
+
+plot(all_models[[28]])
+all_models_red[[30]]$F1_train
+all_models[[29]]$table
+
+# ANN
+
+plot(all_models[[31]])
+all_models_red[[33]]$F1_train
+all_models[[32]]$table
+
+# Tuning parameters are redefined for SVM linear, Boosted decision tree and Random forest
+
+# SVM Linear
+tuneGrids[[4]] <- data.frame(C = seq(0.5, 100, 0.5))
+
+# Boosted decision tree
+tuneGrids[[9]] <- data.frame(nIter = seq(1, 19, 2),
+                             method = "M1")
+
+# Random forest
+tuneGrids[[10]] <- data.frame(mtry = seq(1, 20, 1))
+
+all_models <- mapply(calc_model, methods, tuneGrids)
+
+# Tuning results are checked again
+
+# SVM Linear
+
+all_models_red[[12]]$F1_train
+all_models_red[[11]]$table
+plot(all_models_red[[10]])
+
+# Boosted decision tree
+
+all_models_red[[27]]$F1_train
+all_models_red[[26]]$table
+plot(all_models_red[[25]])
+
+# Random forest
+
+all_models_red[[30]]$F1_train
+all_models_red[[29]]$table
+plot(all_models_red[[28]])
+
 
 # Performance of the best model
 svmRadial_pred <- predict(all_models_red[[13]], test)
 svmRadial_cm <- confusionMatrix(svmRadial_pred, test$DEATH_EVENT, mode = "everything", positive = "Yes")
 svmRadial_cm
 
-# 10-fold cross-validation will be used and comapred to the 5-fold c-v
+# Performance of all models
 
-fitControl <- trainControl(method = "cv",
-                           number = 10,
-                           p = 0.9,
-                           classProbs = TRUE,
-                           summaryFunction = f1)
-
-all_models_red <- mapply(calc_model_red, methods, tuneGrids)
-df_red <- bind_rows(all_models_red[seq(3, 33, 3)], .id = "column_label")
-rownames(df_red) <- NULL
-
-# df_red <- df_red[, c(2, 3)]
-df_red
+all_models_eval <- function(fit) {
+  pred <- predict(fit, test)
+  cm <- confusionMatrix(pred, test$DEATH_EVENT, mode = "everything", positive = "Yes")
+  df <- cm$byClass["F1"]
+}
+all_results <- bind_rows(lapply(all_models_red[seq(1, 31, 3)], all_models_eval), .id = "column_label")
+all_results <- cbind(df_red[c(1,2)], all_results[2])
+arrange(all_results, desc(F1))
 
 #############################################################################
-# Ensemble learning: Ensemble of 11 models will be used to improve prediction
+# Ensemble learning: Ensemble of 7 models will be used to improve prediction
 #############################################################################
 
 # Predictions for all models are combined in a dataframe with pred_all function
@@ -433,6 +459,9 @@ pred_all <- function(method, tuneGrid) {
 all <- mapply(pred_all, methods, tuneGrids)
 all <- as.data.frame(all)
 
+# Models with strong overtraining are excluded
+all <- all[,-c(3, 8, 9, 10)]
+
 # Output values are transformed to 1s and 0s
 all <- ifelse(all == "Yes", 1, 0)
 
@@ -442,26 +471,29 @@ all_sums
 
 # The function ensemble takes a vote threshold needed for a positive or negative vote
 ensemble <- function(vote) {
-  all_voted <- ifelse(all_sums > vote, 1, 0)
+  all_voted <- ifelse(all_sums >= vote, 1, 0)
   all_voted <- as.factor(all_voted)
   levels(all_voted) = c("No", "Yes")
   cm_voted <- confusionMatrix(all_voted, train$DEATH_EVENT)
   return(cm_voted$byClass["F1"])
 }
 
-# Vote thresholds 1-11 are tried and corresponding F1-scores are calculated
-vote <- seq(1, 11, 1)
+# Vote thresholds 1-7 are tried and corresponding F1-scores are calculated
+vote <- seq(1, 7, 1)
 F1_ens <- sapply(vote, ensemble)
 
 # Relationship between F1-scores and vote thresholds
 df_ens <- data.frame(vote = vote, F1_ens = F1_ens)
 df_ens %>%
   ggplot(aes(vote, F1_ens)) +
-  geom_line()
+  geom_line() +
+  xlab("Vote threshold") +
+  ylab("F1 ensemble") +
+  scale_x_continuous(breaks = seq(1, 7, 1))
 
-# Vote threshold 3 gives the best prediction, higher than that of the single models
+# Vote threshold 1 gives the best prediction, higher than that of the single models
 
-all_voted <- ifelse(all_sums > 3, 1, 0)
+all_voted <- ifelse(all_sums >= 1, 1, 0)
 all_voted <- as.factor(all_voted)
 levels(all_voted) = c("No", "Yes")
 cm_voted <- confusionMatrix(all_voted, train$DEATH_EVENT)
@@ -483,16 +515,18 @@ pred_all_test <- function(method, tuneGrid) {
   return(df)
 }
 
-all_test <- mapply(pred_all_test, methods, tuneGrids)
+silent <- capture.output(
+  all_test <- mapply(pred_all_test, methods, tuneGrids))
 all_test <- as.data.frame(all_test)
+all_test <- all_test[,-c(3, 8, 9, 10)]
+
 all_test <- ifelse(all_test == "Yes", 1, 0)
-all_sums_test <- rowSums(all_test)
-all_sums_test
+all_test_sums <- rowSums(all_test)
 
-all_voted_test <- ifelse(all_sums_test > 3, 1, 0)
-all_voted_test <- as.factor(all_voted_test)
-levels(all_voted_test) = c("No", "Yes")
-cm_voted_test <- confusionMatrix(all_voted_test, test$DEATH_EVENT)
-cm_voted_test$byClass["F1"]
-cm_voted_test
+all_test_voted <- ifelse(all_test_sums >= 1, 1, 0)
+all_test_voted <- as.factor(all_test_voted)
+levels(all_test_voted) = c("No", "Yes")
+cm_test_voted <- confusionMatrix(all_test_voted, test$DEATH_EVENT, positive = "Yes")
+cm_test_voted$byClass["F1"]
 
+# Ensemble does not result in a better prediction
